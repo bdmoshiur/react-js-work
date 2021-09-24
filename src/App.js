d9a7bd5a4de40ac620bcd4ac21cdf9fbfb8b1fe2
moshiur rahman <moshiurcse888@gmail.com>
2021-09-24 10:57:45 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from './Components/Card';
+import Card2 from './Components/Card2';
+import State from './Components/State';
 import Data from './data.json'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <div>
             <h2 className='headingclass'>Hello Test</h2>
               {items}
+              <Card2 name="Hello From Card Two"/>
+              <State />
         </div>
     );
 }
